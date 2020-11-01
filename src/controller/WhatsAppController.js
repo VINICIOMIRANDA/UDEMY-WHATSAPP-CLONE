4a92d0647fe3d9b5c5b67eb23eb43861c36e11b7
vinicio.covalski <vinicio.covalski@gmail.com>
2020-11-01 01:35:20 -0300

    116. W05 - Obtendo dados dos painéis e usando FormData

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -23,128 +23,184 @@ class WhatsAppController {
 
     }
 
-    elementsPrototype(){
+    elementsPrototype() {
 
-          //Tratando o escopo na função;
+        //Tratando o escopo na função;
 
-        Element.prototype.hide = function(){
+        Element.prototype.hide = function () {
 
             this.style.display = 'none';
             return this;
-          }
+        }
 
-        Element.prototype.show = function(){
+        Element.prototype.show = function () {
 
             this.style.display = 'block';
             return this; // |Retornar o app para que o proximo metodo execute no elemento 
 
-          }
+        }
 
-        Element.prototype.toggle = function(){
+        Element.prototype.toggle = function () {
 
             this.style.display = (this.style.display === 'none') ? 'block' : 'none';
             return this;
-          }
+        }
 
-          Element.prototype.on = function(events, fn){
+        Element.prototype.on = function (events, fn) {
 
-            events.split(' ').forEach(event=>{
+            events.split(' ').forEach(event => {
 
                 this.addEventListener(event, fn);
 
             });
             return this;
 
-          }
+        }
 
-          Element.prototype.css = function(styles){
+        Element.prototype.css = function (styles) {
 
-            for(let name in styles) {
+            for (let name in styles) {
 
-                this.style[name] = styles[name]; 
+                this.style[name] = styles[name];
 
             }
             return this;
 
-          }
+        }
 
-          Element.prototype.addClass = function(name){
+        Element.prototype.addClass = function (name) {
 
             this.classList.add(name);
             return this;
 
-          }
+        }
 
-          Element.prototype.removeClass = function(name){
+        Element.prototype.removeClass = function (name) {
 
             this.classList.remove(name);
             return this;
 
-          }
+        }
 
-          Element.prototype.toggleClass = function(name){
+        Element.prototype.toggleClass = function (name) {
 
             this.classList.toogle(name);
             return this;
 
 
-          }
+        }
 
-          Element.prototype.hasClass = function(name){
+        Element.prototype.hasClass = function (name) {
 
-            return  this.classList.contains(name); // Se tem ou não
+            return this.classList.contains(name); // Contains "Se tem ou não"
 
-          }
+        }
 
- 
+        HTMLFormElement.prototype.getForm = function () { //Aula 116
+
+            return new FormData(this);
+
+        }
+
+        HTMLFormElement.prototype.toJSON = function () { //Aula 116 
+
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+
+                json[key] = value
+
+            });
+
+
+            return json; // Gerar o formulario preenchido no formato JSON
+
+        }
+
+
     }
 
-    initEvents(){
+    initEvents() {
 
-        this.el.myPhoto.on('click', e=>{
-            
+        this.el.myPhoto.on('click', e => {
+
             this.closeAllLeftPanel(); //Fechando o panel
             this.el.panelEditProfile.show();
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.el.panelEditProfile.addClass('open');
-            },300);
-      
+            }, 300);
+
         });
 
-        this.el.btnNewContact.on('click', e=> {
+        this.el.btnNewContact.on('click', e => {
 
             this.closeAllLeftPanel();
             this.el.panelAddContact.show();
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.el.panelAddContact.addClass('open');
-            },300);
+            }, 300);
 
         });
 
-        this.el.btnClosePanelEditProfile.on('click', e=>{
+        this.el.btnClosePanelEditProfile.on('click', e => {
 
             this.el.panelEditProfile.removeClass('open');
 
         });
 
-        this.el.btnClosePanelAddContact.on('click', e=>{
+        this.el.btnClosePanelAddContact.on('click', e => {
 
             this.el.panelAddContact.removeClass('open');
 
 
-        })
+        });
 
-        
+        this.el.photoContainerEditProfile.on('click', e => {
 
-      }
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => { // keypress 
+
+            if (e.key === 'Enter') {
+
+                e.preventDefault(); //Não atualizar
+                this.el.btnSavePanelEditProfile.click();
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
 
 
-        closeAllLeftPanel(){
+        });
 
-            this.el.panelAddContact.hide();
+        this.el.formPanelAddContact.on('submit', e => {
 
-            this.el.panelEditProfile.hide();
+            e.preventDefault(); //Não atualizar
 
-        }
+            let formData = new FormData(this.el.formPanelAddContact); //Colocando o ID do elmento no construtor do FormData.
+
+
+
+
+        });
+
+
+
+
+    }
+
+
+    closeAllLeftPanel() {
+
+        this.el.panelAddContact.hide();
+
+        this.el.panelEditProfile.hide();
+
+    }
 
 }
