f9b5ecc8c08131de2977f35dced2aaf1b10841d3
vinicio.covalski <vinicio.covalski@gmail.com>
2020-11-02 00:12:04 -0300

    134. W23 - Gravando áudio capturado pelo microfone com MediaRecorder

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -1,10 +1,14 @@
 import { ClassEvent } from "../util/ClassEvent";
 
-export class MicrophoneController  extends ClassEvent{
+export class MicrophoneController extends ClassEvent {
 
     constructor() {
 
         super(); //Chama o construtor do PAI dele, de quem ele está extends ClassEvent>constructor <Herança>
+
+        this._mimeType = 'audio/webm';
+
+        this._available = false; //Variavel criada para identificar se o audio está disponóveç para gravar
 
         navigator.mediaDevices.getUserMedia({
 
@@ -12,16 +16,17 @@ export class MicrophoneController  extends ClassEvent{
 
         }).then(stream => {
 
+            this._available = true;
 
             this._stream = stream;
 
-            let audio = new Audio();
+            //   let audio = new Audio();
             // audio.src = URL.createObjectURL(stream);
 
-            audio.srcObject = stream;
-            audio.play();
+            //  audio.srcObject = stream;
+            //   audio.play();
 
-            this.trigger('play', audio);
+            this.trigger('ready', this._stream);
 
         }).catch(err => {
 
@@ -32,6 +37,12 @@ export class MicrophoneController  extends ClassEvent{
 
     }
 
+    isAvailable() {
+
+        return this._available;
+
+    }
+
     stop() {
 
         this._stream.getTracks().forEach(track => {
@@ -39,6 +50,79 @@ export class MicrophoneController  extends ClassEvent{
             track.stop();
 
         });
+
+    }
+
+    startRecorder() {
+
+        if (this.isAvailable()) {
+
+            this._mediaRecorder = new MediaRecorder(this._stream, {
+
+                mimeType: this._mimeType
+
+            });
+
+            this._recorderChunks = [];
+
+            this._mediaRecorder.addEventListener('dataavailable', e => {
+
+                if (e.data.size > 0) this._recorderChunks.push(e.data);
+
+            });
+
+            this._mediaRecorder.addEventListener('stop', e => {
+
+                let blob = new Blob(this._recorderChunks, {
+                    type: this._mimeType
+                });
+
+                let filename = `re${Date.now()}.webm`;
+
+                let file = new File([blob], filename, {
+
+                    type: this._mimeType,
+                    lastModified: Date.now()
+                });
+
+                console.log('file', file);
+
+                let reader = new FileReader();
+
+                reader.onload = e => { // Para ouvir o áudio.
+
+                    console.log('reader file', file);
+
+                    let audio = new Audio(reader.result);
+
+                    audio.play(); //tocando audio.
+
+                }
+                reader.readAsDataURL(file);
+
+            });
+
+            this._mediaRecorder.start();
+
+        }
+
+
+
+    }
+
+    stopRecorder() {
+
+        if (this.isAvailable()) {
+
+            this._mediaRecorder.stop();
+            this.stop();
+
+
+
+
+        }
+
+
 
     }
 
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -408,25 +408,30 @@ export class WhatsAppController {
 
             this._microphoneController = new MicrophoneController();
 
-            this._microphoneController.on('play', audio => {
 
-                console.log('Recebi o Evento play', audio);
+            this._microphoneController.on('ready', audio => {
+
+                console.log('ready event');
+
+                this._microphoneController.startRecorder();
+
+                //   console.log('Recebi o evento play', audio);
 
             });
-    
+
         });
 
 
         this.el.btnCancelMicrophone.on('click', e => {
 
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
         });
 
         this.el.btnFinishMicrophone.on('click', e => {
 
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
         });
