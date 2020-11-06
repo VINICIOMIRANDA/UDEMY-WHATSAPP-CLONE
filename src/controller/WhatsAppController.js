16092f395eeba4bb68ed5de08803c658c1325298
vinicio.covalski <vinicio.covalski@gmail.com>
2020-11-05 23:24:53 -0300

    148. W37 - Descendo o Scroll automaticamente se estiver encostado em baixo.

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -215,18 +215,26 @@ export class WhatsAppController {
 
         });
 
+        this.el.panelMessagesContainer.innerHTML = '';
+
 
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
         .onSnapshot(docs => {
   
-          this.el.panelMessagesContainer.innerHTML = '';
+
+
+          let scrollTop = this.el.panelMessagesContainer.scrollTop;
+          let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+          let autoScroll = (scrollTop >= scrollTopMax );
   
           docs.forEach(doc => {
   
             let data = doc.data();
             data.id = doc.id;
   
-            if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
+            if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) { // O nome de seletores  ID não pode começar com o número por isso #_
+
+           
   
               let message = new Message();
   
@@ -237,11 +245,22 @@ export class WhatsAppController {
               let view = message.getViewElement(me);
   
               this.el.panelMessagesContainer.appendChild(view);
+
+            
   
             }
   
          
             });
+
+            if (autoScroll) {
+
+                this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+
+              } else {
+
+                this.el.panelMessagesContainer.scrollTop = scrollTop;
+              }
 
         });
 
